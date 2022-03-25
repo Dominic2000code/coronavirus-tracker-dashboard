49fb5f8b2c31ef1fdeca52702a6c286366c38c1c
Dominic Bluawofogbe <bluawofogbedominic@gmail.com>
2022-03-25 15:50:43 +0000

    added a Maptiler url and attribution, changed map marker

--- src/Map.js
+++ src/Map.js
@@ -1,19 +1,31 @@
 import React from "react";
 import "./Map.css";
-import { MapContainer as LeafletMap, TileLayer } from "react-leaflet";
+import { MapContainer as LeafletMap, TileLayer, useMap } from "react-leaflet";
+// import L from "leaflet";
 import { showDataOnMap } from "./utils";
 
+import "leaflet/dist/leaflet.css";
+
 const Map = ({ countries, casesType, center, zoom }) => {
+	function ChangeView({ center, zoom }) {
+		const map = useMap();
+		map.setView(center, zoom);
+		return null;
+	}
 	return (
-		<div className="map">
-			<LeafletMap center={center} zoom={zoom}>
-				<TileLayer
-					url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-					attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-				/>
-				{showDataOnMap(countries, casesType)}
-			</LeafletMap>
-		</div>
+		<LeafletMap
+			className="map"
+			center={center}
+			zoom={zoom}
+			scrollWheelZoom={false}
+		>
+			<ChangeView center={center} zoom={zoom} />
+			<TileLayer
+				url="https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=DlIebgCmxNwwlxrCYBrK"
+				attribution='<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
+			/>
+			{showDataOnMap(countries, casesType)}
+		</LeafletMap>
 	);
 };
 
