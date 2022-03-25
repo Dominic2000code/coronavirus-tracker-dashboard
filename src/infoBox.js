2e51fb8c04dc55433cca0821182daf2ed3ba6c81
Dominic Bluawofogbe <bluawofogbedominic@gmail.com>
2022-03-25 15:49:45 +0000

    added a spinner for the infoBox with some style

--- src/infoBox.js
+++ src/infoBox.js
@@ -3,27 +3,39 @@ import { Card, CardContent, Typography } from "@material-ui/core";
 import "./infoBox.css";
 
 function InfoBox({ title, cases, total, active, isRed, ...props }) {
-    // console.log(title, active);
-    return (
-        <Card
-            onClick={props.onClick}
-            className={`infoBox ${active && "infoBox--selected"} ${isRed && "infoBox--red"
-                }`}
-        >
-            <CardContent>
-                <Typography color="textSecondary" gutterBottom>
-                    {title}
-                </Typography>
-                <h2 className={`infoBox__cases ${!isRed && "infoBox__cases--green"}`}>
-                    {cases}
-                </h2>
+	// console.log(title, active);
+	return (
+		<Card
+			onClick={props.onClick}
+			className={`infoBox ${active && "infoBox--selected"} ${
+				isRed && "infoBox--red"
+			}`}
+		>
+			<CardContent>
+				<Typography color="textSecondary" gutterBottom>
+					{title}
+				</Typography>
+				<h2
+					className={`infoBox__cases ${
+						!isRed && "infoBox__cases--green"
+					}`}
+				>
+					{props.isLoading ? (
+						<i
+							className="fa fa-spinner fa-spin"
+							style={{ fontSize: "24px" }}
+						/>
+					) : (
+						cases
+					)}
+				</h2>
 
-                <Typography className="infoBox__total" color="textSecondary">
-                    {total} Total
-                </Typography>
-            </CardContent>
-        </Card>
-    );
+				<Typography className="infoBox__total" color="textSecondary">
+					{total} Total
+				</Typography>
+			</CardContent>
+		</Card>
+	);
 }
 
 export default InfoBox;
