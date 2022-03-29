ca51ac57881c7963e5e63d13311f51a9d6825bd5
Dominic Bluawofogbe <bluawofogbedominic@gmail.com>
2022-03-29 16:51:48 +0000

    added modal for country stats

--- src/Table.js
+++ src/Table.js
@@ -1,30 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Table.css";
 import numeral from "numeral";
+import CountryGraphContainer from "./country_charts/country_graph";
 
-function Table({ countries, tableState }) {
+function Table({ countries, tableState, timeSeries, countries_, totalInt }) {
+	const [showModal, setShowModal] = useState("hidden");
+	const [countryClicked, setCountryClicked] = useState("");
+
+	const onRowClick = (country) => {
+		setCountryClicked(country);
+		setShowModal("show-modal");
+	};
+
 	// console.log(countries);
 	return (
-		<div className="table">
-			<table>
-				<tbody>
-					{countries.map(({ country, cases, deaths, recovered }) => (
-						<tr>
-							<td> {country} </td>
-							<td>
-								<strong>
-									{tableState === "Cases"
-										? numeral(cases).format("0,0")
-										: tableState === "Recovered"
-										? numeral(recovered).format("0,0")
-										: numeral(deaths).format("0,0")}
-								</strong>
-							</td>
-						</tr>
-					))}
-				</tbody>
-			</table>
-		</div>
+		<React.Fragment>
+			<div className={`modal ${showModal} `}>
+				<button
+					onClick={() => setShowModal("hidden")}
+					className="close-modal"
+				>
+					&times;
+				</button>
+				<div id="graph-container">
+					<br></br>
+					<CountryGraphContainer
+						country={countryClicked}
+						countries={countries_}
+						total={totalInt}
+						timeSeries={timeSeries}
+					/>
+				</div>
+			</div>
+			{/* <div className={`overlay ${showModal}`}></div> */}
+
+			<div className="table">
+				<table>
+					<tbody>
+						{countries.map(
+							({ country, cases, deaths, recovered }) => (
+								<tr onClick={() => onRowClick(country)}>
+									<td> {country} </td>
+									<td>
+										<strong>
+											{tableState === "Cases"
+												? numeral(cases).format("0,0")
+												: tableState === "Recovered"
+												? numeral(recovered).format(
+														"0,0"
+												  )
+												: numeral(deaths).format("0,0")}
+										</strong>
+									</td>
+								</tr>
+							)
+						)}
+					</tbody>
+				</table>
+			</div>
+		</React.Fragment>
 	);
 }
 
